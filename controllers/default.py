62765f7573c8f6a4c0afd0713d6df14c7878fbce
revanthb3000 <revanthb3000@gmail.com>
2014-07-04 14:21:07 +0530

    Added image files from the old website and have added the index page

--- controllers/default.py
+++ controllers/default.py
@@ -1,13 +1,4 @@
-# -*- coding: utf-8 -*-
-# this file is released under public domain and you can use without limitations
-
-#########################################################################
-## This is a sample controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-## - call exposes all registered services (none by default)
-#########################################################################
+#Dead code to get intellisense to work.
 if 0:
     from modules import *
     from gluon import *
@@ -19,18 +10,11 @@ if 0:
     crud = Crud()
 
 import databaseConnectionStrings
+import datetime
 
 def index():
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
-
-    if you need a simple wiki simply replace the two lines below with:
-    return auth.wiki()
-    """
-    print databaseConnectionStrings.connectionString
-    response.flash = T("Welcome to web2py!")
-    return dict(message=T('Hello World'))
+    curDate = datetime.datetime.now()
+    return dict(year = curDate.year)
 
 
 def user():
